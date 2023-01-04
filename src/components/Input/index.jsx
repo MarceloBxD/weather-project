87eb9862796f227a07bb1b6daa67722835f37bf4
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-01-04 14:24:57 -0300

    can send requisition to api with pressing enter, creating new components

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,11 +3,18 @@ import * as C from "./styles";
 import { useApp } from "../../contexts/context";
 
 export const Input = () => {
-  const { setCityName } = useApp();
+  const { setCityName, getWeatherData } = useApp();
+
+  const handleEnter = (e) => {
+    if (e.keyCode === 13) {
+      getWeatherData();
+    }
+  };
 
   return (
     <C.InputArea>
       <C.Input
+        onKeyDown={handleEnter}
         onChange={(e) => setCityName(e.target.value)}
         placeholder="Cidade"
       />
